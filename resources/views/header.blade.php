e0418c849086ba11ec0f350a750018124cf79678
Bayero Abass Barry <bayeroabassb@gmail.com>
2018-10-15 21:51:58 +0000

    fin contact

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,16 +9,22 @@
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title -->
-    <title>Mosh - Creative Business Template | Contact</title>
+    <title>N-technology- Creative Business Template | Contact</title>
 
     <!-- Favicon -->
     <link rel="icon" href="img/core-img/favicon.ico">
 
     <!-- Core Stylesheet -->
-    <link href="style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- Responsive CSS -->
     <link href="css/responsive.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/omnes-pro.css" rel="stylesheet">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/magnific-popup.css" rel="stylesheet">
+    <link href="css/animatesss.css" rel="stylesheet">
 
 </head>
 
@@ -65,7 +71,97 @@
         </div>
     </header>
     <!-- ***** Header Area End ***** -->
-    @yield('content')
+    <div>
+         @yield('content')
+    </div>
+        <!-- ***** Footer Area Start ***** -->
+    <footer class="footer-area clearfix">
+        <!-- Top Fotter Area -->
+        <div class="top-footer-area section_padding_100_0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-lg-5">
+                        <div class="single-footer-widget mb-100">
+                            <a href="#" class="mb-50 d-block"><img src="img/core-img/logo.png" alt=""></a>
+                            <p>N-technology est une entreprise de technologie fournissant aux clients les solutions Informatiques pour la construction de leur business sur le web et le mobile</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-lg-4">
+                        <div class="single-footer-widget mb-100">
+                            <h5>Liens Utiles</h5>
+                            <ul>
+                                <li><a href="#">Accueil</a></li>
+                                <li><a href="#">A Propos</a></li>
+                                <li><a href="#">Nos Services</a></li>
+                                <li><a href="#">Portfolio</a></li>
+                                <li><a href="{{ Route('contact') }}">Nous Contacter</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-10 col-lg-3">
+                        <div class="single-footer-widget mb-100">
+                            <h5>Nos Contacts</h5>
+                            <div class="footer-single-contact-info d-flex">
+                                <div class="contact-icon">
+                                    <img src="img/core-img/map.png" alt="">
+                                </div>
+                                <p>Dixinn-Landréa, Conakry, Guinée</p>
+                            </div>
+                            <div class="footer-single-contact-info d-flex">
+                                <div class="contact-icon">
+                                    <img src="img/core-img/call.png" alt="">
+                                </div>
+                                <p>Portable: +224 622 30 15 56 <br> Bureau: +224 664 65 28 21</p>
+                            </div>
+                            <div class="footer-single-contact-info d-flex">
+                                <div class="contact-icon">
+                                    <img src="img/core-img/message.png" alt="">
+                                </div>
+                                <p>office@ntechnology.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Fotter Bottom Area -->
+        <div class="footer-bottom-area">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="footer-bottom-content h-100 d-md-flex justify-content-between align-items-center">
+                            <div class="copyright-text">
+                                <p style="text-align: center;">
+                                     &copy; <script>document.write(new Date().getFullYear());</script><strong> N-technology</strong> - Tous Droits Réservés
+
+                                </p>
+                            </div>
+                            <div class="footer-social-info">
+                                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ***** Footer Area End ***** -->
+
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+    <!-- Google Maps -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwuyLRa1uKNtbgx6xAJVmWy-zADgegA2s"></script>
+    <script src="js/map-active.js"></script>
 
 </body>
 </html>
